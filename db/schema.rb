a9688c7c35198e5aad3e41d2b0cdf601c75c9d0b
ashtoncoghlan <ashton.coghlan@gmail.com>
2014-11-17 15:59:46 +0800

    added user_id to annotations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141114095454) do
+ActiveRecord::Schema.define(version: 20141117075853) do
 
   create_table "annotations", force: true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141114095454) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "stock_trackers", force: true do |t|
