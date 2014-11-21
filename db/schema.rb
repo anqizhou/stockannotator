63397b5674c81ecdeb8e8b876e3e01b156ef0c36
Mark Wilson <secretchinamark@gmail.com>
2014-11-21 12:53:17 +0800

    Change StockTicker model to Stock and associate with users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117084514) do
+ActiveRecord::Schema.define(version: 20141121042049) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "annotations", force: true do |t|
     t.string   "title"
@@ -22,12 +25,15 @@ ActiveRecord::Schema.define(version: 20141117084514) do
     t.integer  "user_id"
   end
 
-  create_table "stock_trackers", force: true do |t|
+  create_table "stocks", force: true do |t|
     t.string   "ticker"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.date     "date"
-    t.float    "price"
+  end
+
+  create_table "stocks_users", id: false, force: true do |t|
+    t.integer "stock_id", null: false
+    t.integer "user_id",  null: false
   end
 
   create_table "users", force: true do |t|
@@ -48,7 +54,7 @@ ActiveRecord::Schema.define(version: 20141117084514) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
